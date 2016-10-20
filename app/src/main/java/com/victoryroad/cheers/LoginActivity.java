b42507bb090121d67c6a5f3bc68a57ddeacaef70
Kyle Rohlfing <krohlfing94@gmail.com>
2016-10-19 22:16:50 -0500

    placed facebook stuff into its own fragmetn

--- app/src/main/java/com/victoryroad/cheers/LoginActivity.java
+++ app/src/main/java/com/victoryroad/cheers/LoginActivity.java
@@ -1,22 +1,70 @@
 package com.victoryroad.cheers;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Build;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
-public class LoginActivity extends AppCompatActivity {
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+public class LoginActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener {
+    private LoginFragment loginFragment;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+
+        hideStatusBar();
+        setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+        TextView titleTextView = (TextView) findViewById(R.id.title_text_view);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/appo_paint.otf");
+        titleTextView.setTypeface(typeface);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void hideStatusBar() {
         if (Build.VERSION.SDK_INT < 16) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -32,10 +80,10 @@ public class LoginActivity extends AppCompatActivity {
                 actionBar.hide();
             }
         }
-        setContentView(R.layout.activity_login);
+    }
 
-        TextView titleTextView = (TextView) findViewById(R.id.title_text_view);
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/appo_paint.otf");
-        titleTextView.setTypeface(typeface);
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
