2d96a865d4ab47e8a4fd782ee33f5e772a6be4c5
Kyle Rohlfing <krohlfing94@gmail.com>
2016-10-25 01:23:22 -0500

    Now when you select a list element it slides out of the way exposing a sublist.

--- app/src/main/java/com/victoryroad/cheers/AddDrinkActivity.java
+++ app/src/main/java/com/victoryroad/cheers/AddDrinkActivity.java
@@ -1,21 +1,16 @@
 package com.victoryroad.cheers;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.Menu;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.TextView;
 
 import java.io.Console;
 
-public class AddDrinkActivity extends AppCompatActivity {
+public class AddDrinkActivity extends AppCompatActivity implements AlcoholCategoriesFragment.OnFragmentInteractionListener, SpiritsListFragment.OnFragmentInteractionListener, EmptyFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +19,11 @@ public class AddDrinkActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-    }
+
+        AlcoholCategoriesFragment alcoholCategoriesFragment = new AlcoholCategoriesFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.alcoholList, alcoholCategoriesFragment);
+        transaction.commit();    }
 
     public boolean onOptionsItemSelected(MenuItem item){
         if (item.getItemId() == android.R.id.home) {
@@ -32,5 +31,10 @@ public class AddDrinkActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
