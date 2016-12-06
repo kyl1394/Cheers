3de7fdb30d667566f33bc076ac51c56c0ffc4829
Kyle Rohlfing <krohlfing94@gmail.com>
2016-12-05 22:47:11 -0600

    Can now call your personal contact when you are done with your night out.

---
+++ app/src/main/java/com/victoryroad/cheers/CallPersonalContactActivity.java
@@ -0,0 +1,22 @@
+package com.victoryroad.cheers;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.victoryroad.cheers.dataclasses.Contact;
+import com.victoryroad.cheers.dataclasses.Settings;
+
+public class CallPersonalContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        Contact contact = Settings.getSettingsFor(getApplicationContext()).getContact();
+//
+//
+//        startActivity(intent);
+    }
+}
--- app/src/main/java/com/victoryroad/cheers/Notification_receiver.java
+++ app/src/main/java/com/victoryroad/cheers/Notification_receiver.java
@@ -1,12 +1,16 @@
 package com.victoryroad.cheers;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.RemoteInput;
+
+import com.victoryroad.cheers.dataclasses.Settings;
 
 /**
  * Created by krohlfing on 12/5/2016.
@@ -24,7 +28,9 @@ public class Notification_receiver extends BroadcastReceiver{
         repeating_intent2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         PendingIntent pendingIntent = RequestUberHomeActivity.getDismissIntent(100, context);
-        PendingIntent callDDIntent = PendingIntent.getActivity(context, 101, repeating_intent2, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent callDD = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", Settings.getSettingsFor(context).getContact().number, null));
+        PendingIntent callDDIntent = PendingIntent.getActivity(context, 101, callDD, PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setContentIntent(pendingIntent)
@@ -32,10 +38,14 @@ public class Notification_receiver extends BroadcastReceiver{
                 .setContentTitle("Heading Home?")
                 .setContentText("Let's make sure you get there safely.")
                 .setVibrate(new long[] {1000, 1000})
-                .setAutoCancel(true);
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true);
 
         builder.mActions.add(new NotificationCompat.Action(R.drawable.addbeer, "Request Uber", pendingIntent));
         builder.mActions.add(new NotificationCompat.Action(R.drawable.addbeer, "Call Designated Driver", callDDIntent));
-        notificationManager.notify(100, builder.build());
+
+        Notification notification = builder.build();
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(100, notification);
     }
 }
